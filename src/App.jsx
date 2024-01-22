2dc9d5f35ea0c814c90b0a8cc2dd4b34cf0eff49
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-01-22 16:00:29 +0530

    project setup - basic

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-center text-2xl pt-5">Currency Converter App</h1>
+    </>
+  );
+}
+
+export default App;
